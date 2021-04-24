23d7b924c0b75ade75c4123cdfba12a093a8fbb4
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-04-24 20:06:41 +0900

    views.py編集

--- api/views.py
+++ api/views.py
@@ -1,6 +1,19 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from rest_framework import viewsets
+from .serializers import TaskSerializer, UserSerializer
+from .models import Task
 
-from django.shortcuts import render
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+class MyProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    def get_object(self):
+        return self.request.user
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
